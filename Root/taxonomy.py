d831fe247a4e46ee5c546bc1245267d1b5b8493e
Aubrey M <you@example.com>
2024-04-13 15:57:29 -0500

    Trying to test the methods of taxonomy class

--- Root/taxonomy.py
+++ Root/taxonomy.py
@@ -1,8 +1,15 @@
 class taxonomy:
     def __init__(self):
-        self.taxonomy = {}
+        self.t = []
     #
     def addTaxonomy(self, taxonomy):
         self.taxonomy.apprend(taxonomy)
     #
+    def print(self):
+        return print(self.taxonomy)
+    #
+    x = 1
+    print(x)
+    x = taxonomy()
+    print(x)
 #
